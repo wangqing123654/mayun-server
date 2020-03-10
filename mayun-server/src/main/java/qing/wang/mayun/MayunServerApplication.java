ca7e26d029c3ea329aa67f31e5cdae2ef8b2ec8a
wangqing123654 <49476494+wangqing123654@users.noreply.github.com>
2020-03-11 00:42:10 +0800

    11

---
+++ mayun-server/src/main/java/qing/wang/mayun/MayunServerApplication.java
@@ -0,0 +1,13 @@
+package qing.wang.mayun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayunServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayunServerApplication.class, args);
+	}
+
+}
